d47f81ff9a2d6a7d6c2fef28213e269709adbfa4
Reid Back <reid.backcnmt@gmail.com>
2024-02-29 20:57:05 +1100

    made login with handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,12 +2,21 @@ const router = require('express').Router();
 // const {blogPost, Comment, User} = require('../models');
 // const withAuth = require('../utils/auth');
 
-router.get('/landing', (req, res) =>{
+router.get('/', (req, res) =>{
     try {
         res.render('landing');
     } catch (error) {
         res.status(500).json(error);
     }
+});
+
+router.get('/login', (req, res) => {
+    try {
+        res.render('login');
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
+
 
 module.exports = router;
