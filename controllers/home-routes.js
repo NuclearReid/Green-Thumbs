16b4f5d3ca44aa6e9d677a15a3beea1ae5d72fc7
Reid Back <reid.backcnmt@gmail.com>
2024-03-03 16:41:04 +1100

    added links and .handlebars to the climates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,10 +30,45 @@ router.get('/signup', (req, res) => {
 router.get('/profile', (req, res) => {
     try {
         res.render('profile')
-    } catch (error) {
-        res.status(500).json(error)
+    } catch (error) {;
     }
 })
+
+router.get('/cool', (req, res) => {
+    try {
+        res.render('cool');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+router.get('/hot', (req, res) => {
+    try {
+        res.render('hot');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+router.get('/subTropic', (req, res) => {
+    try {
+        res.render('subTropic');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+router.get('/tropic', (req, res) => {
+    try {
+        res.render('tropic');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+router.get('/warm', (req, res) => {
+    try {
+        res.render('warm');
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 
 module.exports = router;
