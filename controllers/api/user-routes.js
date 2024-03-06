3dd33b537dc4d7ff01cc6042e7b05ab5d13ec45f
Stacey Rogers <staceyarogers@outlook.com>
2024-03-07 08:45:29 +1100

    Commit profile changes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -17,51 +17,44 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) =>{
+// userRoutes.js
+router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
-
-        // req.session.save(() => {
-        //     req.session.user_id = userData.id;
-        //     req.session.logged_in = true;
-
-        // });
-        res.status(200).json(userData);
-
-        // res.render('profile');
-
+        res.redirect(`/profile/${userData.id}`);
     } catch (error) {
         res.status(500).json(error);
     }
 });
 
+
 // router.post('/login', async (req, res) => {
 //     try {
 //       const userData = await User.findOne({ where: { email: req.body.email } });
-  
+
 //       if (!userData) {
 //         res
 //           .status(400)
 //           .json({ message: 'Incorrect email or password, please try again' });
 //         return;
 //       }
-  
+
 //       const validPassword = await userData.checkPassword(req.body.password);
-  
+
 //       if (!validPassword) {
 //         res
 //           .status(400)
 //           .json({ message: 'Incorrect email or password, please try again' });
 //         return;
 //       }
-  
+
 //       req.session.save(() => {
 //         req.session.user_id = userData.id;
 //         req.session.logged_in = true;
-        
+
 //         res.json({ user: userData, message: 'You are now logged in!' });
 //       });
-  
+
 //     } catch (err) {
 //       res.status(400).json(err);
 //     }
@@ -78,6 +71,6 @@ router.post('/', async (req, res) =>{
 //       res.status(404).end();
 //     }
 //   });
-  
+
 
 module.exports = router;
