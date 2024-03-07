e6f93a8e2deb9bdcc301b131de3fa5ea69ec9c7a
Reid Back <reid.backcnmt@gmail.com>
2024-03-07 19:44:58 +1100

    made qld endpoint work with handlebars, changed the model by adding get() and set() for location and season

--- controllers/api/plantRoutes.js
+++ controllers/api/plantRoutes.js
@@ -4,9 +4,11 @@ const { Plant } = require('../../models');
 // it's pretty basic, just creates a new entry to the Plant table
 router.post('/newPlant', async (req, res) => {
     try {
+        console.log(req.body);
         const addPlant = await Plant.create(req.body);
         res.status(200).json(addPlant);
     } catch (error) {
+        console.log(error);
         res.status(500).json(error);
     }
 });
