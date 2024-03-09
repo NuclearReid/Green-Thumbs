fc5f9daf9def1401a09e63f509001c0a879b203b
Reid Back <reid.backcnmt@gmail.com>
2024-03-09 18:19:31 +1100

    can now add a new blogpost

---
+++ public/js/newPost.js
@@ -0,0 +1,27 @@
+
+const newPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // gets the values from the new post form
+    const title = document.querySelector('#blog-title').value;
+    const body = document.querySelector('#blog-text').value;
+        
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/blog/', {
+        method: 'POST',
+        // sends the title and content
+        body: JSON.stringify({ title, body }),
+        headers: { 
+            'Content-Type': 'application/json' 
+        },
+      });
+      if (response.ok) {
+        // If successful, redirect the browser to the home page
+        location.reload();
+    }
+  };
+  
+  document
+    .querySelector('.newPost')
+    .addEventListener('submit', newPostFormHandler);
+
