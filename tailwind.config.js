551d3710f66c9215b5110c2eca50dfe9b99f02f3
Yousra <nice.sora@hotmail.com>
2024-03-10 20:44:51 +1100

    changed tailwind to CDN/added header image to profile/ fixed some css

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/**/*.ejs", "./public/**/*.handlebars"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-};
